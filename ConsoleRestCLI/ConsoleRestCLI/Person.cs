ba27f998132e0a1b58242b23fe4830bf13b13d73
pablobt99 <64027415+pablobt99@users.noreply.github.com>
2022-02-02 18:45:18 +0100

    distribuida
    Para diego

---
+++ ConsoleRestCLI/ConsoleRestCLI/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRestCLI
+{
+    public class Person
+    {
+        public long ID { get; set; }
+        public String LastName { get; set; }
+        public String FirstName { get; set; }
+        public String PayRate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
